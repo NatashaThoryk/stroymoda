f0567b587238177c2f77b013712c52116e0d2d6d
Natasha <t.natalya@improveit.solutions>
2021-06-22 20:09:45 +0300

    fix component

--- src/components/CharitySection/index.tsx
+++ src/components/CharitySection/index.tsx
@@ -8,7 +8,7 @@ export default ({list = []}) => {
 				<h3 className={classesMain.title}>Charity and sponsorship</h3>
 				<p className={classes.subtitle}>To extend our social profile Stroymoda is linked to several social initiatives and projects by means of charity and sponsoring.</p>
 				<div className={classes.list}>
-					{list.map(item =>
+					{list.map((item: any) =>
 						<div className={classes.item}>
 							<img src={item.img} alt="image"/>
 							<h4>{item.title}</h4>
