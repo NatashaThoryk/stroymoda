e26717ccffae3ce104a18f705020a3cea3157e7e
Stas R <rar.stas@gmail.com>
2021-07-01 22:53:03 +0300

    use layout

--- src/layouts/main/types.ts
+++ src/layouts/main/types.ts
@@ -1,11 +1,12 @@
 
+interface IFooterProps {
+	isBg?: boolean;
+	customClass?: string;
+	isLogo?: boolean;
+}
 export interface IProps {
 	title?: string;
 	fullWidth?: boolean;
 	mainClassName?: string;
-	footerProps?: {
-		isBig?: boolean;
-		customClass?: string;
-		isLogo?: boolean;
-	};
+	footerProps?: IFooterProps;
 }
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -5,6 +5,7 @@ import classes from "../index.module.scss";
 import ProjectsImg from 'components/ProjectsImg';
 import BreadCrumbs from 'components/BreadCrumbs';
 import toVal from "helpers/clsx";
+import Layout from 'layouts/main';
 
 const title = 'Selected Projects'
 const list1 = {
@@ -74,44 +75,43 @@ const breadCrumbs = [
 const Projects = () => {
 
 	return (
-		<>
-			<Header/>
-			<main className={classes.main}>
-				<div className={classes.content_wrapper}>
-					<BreadCrumbs items={breadCrumbs} />
-					<h2 className={classes.title_cap}>{title}</h2>
-				</div>
-				<div className={classes.container}>
-					<div className={classes.main_list_pd}>
-						<div className={toVal(classes.in_list, classes.in_list_center)}>
-							<ProjectsImg isNew={true} size={list1.size} loc={list1.loc} architect={list1.architect} title={list1.title} img={list1.img} />
-							<ProjectsImg isNew={false} size={list2.size} loc={list2.loc} architect={list2.architect} title={list2.title} img={list2.img} />
-							<ProjectsImg isNew={false} size={list3.size} loc={list3.loc} architect={list3.architect} title={list3.title} img={list3.img} />
-							<ProjectsImg isNew={true} size={list4.size} loc={list4.loc} architect={list4.architect} title={list4.title} img={list4.img} />
-						</div>
-						<div className={toVal(classes.in_list, classes.in_list_center)}>
-							<ProjectsImg isNew={false} size={list5.size} loc={list5.loc} architect={list5.architect} title={list5.title} img={list5.img} />
-							<ProjectsImg isNew={true} size={list6.size} loc={list6.loc} architect={list6.architect} title={list6.title} img={list6.img} />
-							<ProjectsImg isNew={false} size={list7.size} loc={list7.loc} architect={list7.architect} title={list7.title} img={list7.img} />
-							<ProjectsImg isNew={true} size={list8.size} loc={list8.loc} architect={list8.architect} title={list8.title} img={list8.img} />
-						</div>
-						<div className={toVal(classes.in_list, classes.in_list_center)}>
-							<ProjectsImg isNew={false} size={list3.size} loc={list3.loc} architect={list3.architect} title={list3.title} img={list3.img} />
-							<ProjectsImg isNew={true} size={list1.size} loc={list1.loc} architect={list1.architect} title={list1.title} img={list1.img} />
-							<ProjectsImg isNew={false} size={list2.size} loc={list2.loc} architect={list2.architect} title={list2.title} img={list2.img} />
-							<ProjectsImg isNew={true} size={list4.size} loc={list4.loc} architect={list4.architect} title={list4.title} img={list4.img} />
-						</div>
-						<div className={toVal(classes.in_list, classes.in_list_center)}>
-							<ProjectsImg isNew={false} size={list5.size} loc={list5.loc} architect={list5.architect} title={list5.title} img={list5.img} />
-							<ProjectsImg isNew={true} size={list6.size} loc={list6.loc} architect={list6.architect} title={list6.title} img={list6.img} />
-							<ProjectsImg isNew={false} size={list7.size} loc={list7.loc} architect={list7.architect} title={list7.title} img={list7.img} />
-							<ProjectsImg isNew={true} size={list8.size} loc={list8.loc} architect={list8.architect} title={list8.title} img={list8.img} />
-						</div>
+		<Layout footerProps={{
+			isBg: true,
+			customClass: 'is_margin'
+		}}>
+			<div className={classes.content_wrapper}>
+				<BreadCrumbs items={breadCrumbs} />
+				<h2 className={classes.title_cap}>{title}</h2>
+			</div>
+			<div className={classes.container}>
+				<div className={classes.main_list_pd}>
+					<div className={toVal(classes.in_list, classes.in_list_center)}>
+						<ProjectsImg isNew={true} size={list1.size} loc={list1.loc} architect={list1.architect} title={list1.title} img={list1.img} />
+						<ProjectsImg isNew={false} size={list2.size} loc={list2.loc} architect={list2.architect} title={list2.title} img={list2.img} />
+						<ProjectsImg isNew={false} size={list3.size} loc={list3.loc} architect={list3.architect} title={list3.title} img={list3.img} />
+						<ProjectsImg isNew={true} size={list4.size} loc={list4.loc} architect={list4.architect} title={list4.title} img={list4.img} />
+					</div>
+					<div className={toVal(classes.in_list, classes.in_list_center)}>
+						<ProjectsImg isNew={false} size={list5.size} loc={list5.loc} architect={list5.architect} title={list5.title} img={list5.img} />
+						<ProjectsImg isNew={true} size={list6.size} loc={list6.loc} architect={list6.architect} title={list6.title} img={list6.img} />
+						<ProjectsImg isNew={false} size={list7.size} loc={list7.loc} architect={list7.architect} title={list7.title} img={list7.img} />
+						<ProjectsImg isNew={true} size={list8.size} loc={list8.loc} architect={list8.architect} title={list8.title} img={list8.img} />
+					</div>
+					<div className={toVal(classes.in_list, classes.in_list_center)}>
+						<ProjectsImg isNew={false} size={list3.size} loc={list3.loc} architect={list3.architect} title={list3.title} img={list3.img} />
+						<ProjectsImg isNew={true} size={list1.size} loc={list1.loc} architect={list1.architect} title={list1.title} img={list1.img} />
+						<ProjectsImg isNew={false} size={list2.size} loc={list2.loc} architect={list2.architect} title={list2.title} img={list2.img} />
+						<ProjectsImg isNew={true} size={list4.size} loc={list4.loc} architect={list4.architect} title={list4.title} img={list4.img} />
+					</div>
+					<div className={toVal(classes.in_list, classes.in_list_center)}>
+						<ProjectsImg isNew={false} size={list5.size} loc={list5.loc} architect={list5.architect} title={list5.title} img={list5.img} />
+						<ProjectsImg isNew={true} size={list6.size} loc={list6.loc} architect={list6.architect} title={list6.title} img={list6.img} />
+						<ProjectsImg isNew={false} size={list7.size} loc={list7.loc} architect={list7.architect} title={list7.title} img={list7.img} />
+						<ProjectsImg isNew={true} size={list8.size} loc={list8.loc} architect={list8.architect} title={list8.title} img={list8.img} />
 					</div>
 				</div>
-			</main>
-			<Footer isBg={true} customClass="is_margin"/>
-			</>
+			</div>
+		</Layout>
 	);
 };
 
