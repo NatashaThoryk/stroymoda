944b60b54536f3147f509d0e6254cac749a729c8
Stas R <rar.stas@gmail.com>
2021-07-01 22:54:10 +0300

    use layout

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Header from 'components/Header';
-import Footer from 'components/Footer';
 import classes from "../index.module.scss";
 import ProjectsImg from 'components/ProjectsImg';
 import BreadCrumbs from 'components/BreadCrumbs';
