8f345f43c58bb2ab5802591a6e8449b5896a80c6
Natasha <t.natalya@improveit.solutions>
2021-07-27 14:55:35 +0300

    mobile

--- src/components/MainSliderProducts/index.tsx
+++ src/components/MainSliderProducts/index.tsx
@@ -1,9 +1,11 @@
 import classes from './mainSliderProducts.module.scss';
-import { IProps } from './types';
+import {IProps} from './types';
 import React, {useEffect, useState} from 'react';
+import {useAppContext} from "../../context/App";
 
 const MainSliderProducts: React.FC<IProps> = ({imageTop = '', imageBottom = ''}) => {
 	const [slideWidth, setSlideWidth] = useState(0);
+	const {isMobile} = useAppContext();
 
 	useEffect(() => {
 		setSlideWidth(window.innerWidth / 2);
@@ -15,13 +17,27 @@ const MainSliderProducts: React.FC<IProps> = ({imageTop = '', imageBottom = ''})
 	};
 
 	return (
-		<div className={classes.full_bg_slider} onMouseMove={handleMouseMove}>
-			<div className={classes.full_bg_slider_hand} style={{ right: slideWidth }}/>
-			<img src={imageBottom} alt="image" className={classes.full_bg_slider_before} />
-			<div  style={{width: slideWidth}} className={classes.full_bg_slider_after}>
-				<img src={imageTop} alt="image"/>
-			</div>
-		</div>
+		<>
+			{isMobile
+				? (
+					<div className={classes.full_bg_slider}  onTouchMove={handleMouseMove}>
+						<div className={classes.full_bg_slider_hand} style={{right: slideWidth}}/>
+						<img src={imageBottom} alt="image" className={classes.full_bg_slider_before}/>
+						<div style={{width: slideWidth}} className={classes.full_bg_slider_after}>
+							<img src={imageTop} alt="image"/>
+						</div>
+					</div>
+				)
+				: (
+					<div className={classes.full_bg_slider} onMouseMove={handleMouseMove}>
+						<div className={classes.full_bg_slider_hand} style={{right: slideWidth}}/>
+						<img src={imageBottom} alt="image" className={classes.full_bg_slider_before}/>
+						<div style={{width: slideWidth}} className={classes.full_bg_slider_after}>
+							<img src={imageTop} alt="image"/>
+						</div>
+					</div>
+				)}
+		</>
 	);
 };
 
