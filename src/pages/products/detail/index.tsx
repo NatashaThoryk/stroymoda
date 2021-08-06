653407c6eb615b964d25a2e3d06ea31831234340
Natasha <t.natalya@improveit.solutions>
2021-08-06 15:53:50 +0300

    add changes

--- src/pages/products/detail/index.tsx
+++ src/pages/products/detail/index.tsx
@@ -56,7 +56,7 @@ const stepList = [
 ];
 const images = [ '/img/products/detail/st1Large.svg', '/img/products/detail/st2Large.svg', '/img/products/detail/st3Large.svg'];
 const images1 = [ '/img/products/detail/st1.1Large.svg', '/img/products/detail/st1.2Large.svg', '/img/products/detail/st1.3Large.svg'];
-const images2 = [ '/img/products/detail/detail1Large.svg', '/img/products/detail/detail2Large.svg', '/img/products/detail/detail3Large.svg'];
+const images2 = [ '/img/products/detail/1.svg', '/img/products/detail/2.svg', '/img/products/detail/1.svg'];
 const stepList1 = [
 	{
 		number: '1.',
@@ -109,7 +109,6 @@ const breadCrumbs = [
 
 const productsDetail = () => {
 	const [isModal, setIsModal] = useState(false);
-
 	return (
 		<Layout footerProps={{
 			isBg: true,
